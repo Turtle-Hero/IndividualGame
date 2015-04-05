e0ebbdb72781886b5b4392a825e5d842a116c4a5
Trevor Warner <twarner714@gmail.com>
2015-04-05 01:10:06 -0500

    v1.01
    -created dynamic brick health that changes every round
    
    -added sound effect when brick is hit
    
    -re-ordered method calling--more organized
    
    -Now loops rounds (can go past round 1)
    
    -Updated score method to add 1+remaining time in round.
    
    -Added endRound method to reset variables

--- AndroidStudioProjects/IndividualGame/app/src/main/java/trevorwarner/individualgame/PlayActivity.java
+++ AndroidStudioProjects/IndividualGame/app/src/main/java/trevorwarner/individualgame/PlayActivity.java
@@ -1,6 +1,9 @@
 package trevorwarner.individualgame;
 
 import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +21,7 @@ public class PlayActivity extends ActionBarActivity {
 
     private int brickTapCount;
     private int brickHealth;
+    private int prevRoundHealth;
     private int totalBrickHealth;
     private int twoThirdsHealth;
     private int oneThirdHealth;
@@ -29,7 +34,9 @@ public class PlayActivity extends ActionBarActivity {
     private String timeMilli;
     private String brickTapString;
     private ImageButton brickButton;
+    int soundID;
     long millis;
+    SoundPool buttonHitSound;
     Timer cdTimer;
     int roundCount = 0;
     private String TAG = " TAG ";
@@ -43,14 +50,22 @@ public class PlayActivity extends ActionBarActivity {
         roundKeeper = (TextView) findViewById(R.id.roundKeeper);
         timeKeeper = (TextView) findViewById(R.id.timeKeeper);
         brickButton = (ImageButton) findViewById(R.id.brickButton);
-        newRound();
-        setHealthMarks();
-        brickButton.setOnClickListener(brickListener);
-
         timeKeeper.setText("10:00");
+        buttonHitSound = new SoundPool(10, AudioManager.STREAM_MUSIC,1);
+        soundID = buttonHitSound.load(this, R.raw.hit_sound, 1);
+        brickButton.setOnClickListener(brickListener);
+        cdTimer = new Timer(10000, 10);
 
-       cdTimer = new Timer(10000, 10);
-       cdTimer.start(); //starts the 10sec countdown
+        newRound();
+
+    }
+
+    public void newRound() {
+        roundCount++;
+        newBrick();
+        roundKeeper.setText("" + roundCount);
+        setHealthMarks();
+        cdTimer.start();
 
     }
 
@@ -59,6 +74,7 @@ public class PlayActivity extends ActionBarActivity {
     View.OnClickListener brickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+           buttonHitSound.play(soundID,1,1,1,0,1);
            brickTapSetter();
            updateBrickHealth();
         }
@@ -90,8 +106,17 @@ public class PlayActivity extends ActionBarActivity {
         }
     }
 
+    public void newBrick() {
+        brickButton.setImageResource(R.drawable.brick);
+    }
+
     public void setHealthMarks() {
-        totalBrickHealth = 3;
+        if (roundCount == 1){
+            totalBrickHealth = 3;
+        }else {
+            totalBrickHealth = prevRoundHealth + (roundCount + (roundCount - 1));
+        }
+        prevRoundHealth = totalBrickHealth;
         brickHealth = totalBrickHealth;
         twoThirdsHealth = (totalBrickHealth / 3) * 2;
         oneThirdHealth = (totalBrickHealth / 3);
@@ -99,46 +124,41 @@ public class PlayActivity extends ActionBarActivity {
     }
 
     public void updateScore() {
-        score++;
+        score = (score + 1) + (int) (millis / 1000);
         scoreKeeper.setText("" + score);
     }
-    //countdown timer accurate to tenths place
+
+    public void endRound() {
+        brickTapCount = 0;
+        brickView.setText("" + 0);
+        totalBrickHealth = totalBrickHealth + (1 + totalBrickHealth/2);
+        brickHealth = totalBrickHealth;
+        newRound();
+    }
+
     public class Timer extends CountDownTimer {
 
         public Timer(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
         }
-            @Override
-            public void onTick(long millisUntilFinished) {
-                millis = millisUntilFinished;
-                timeSec = "" + millis / 1000;
-                timeMilli = "" + (millis / 100);
-                if (timeMilli.length() > 1) {
-                    timeMilli = timeMilli.substring(1);
-                }
-                timeKeeper.setText(timeSec + "." + timeMilli);
-
+        @Override
+        public void onTick(long millisUntilFinished) {
+            millis = millisUntilFinished;
+            timeSec = "" + millis / 1000;
+            timeMilli = "" + (millis / 100);
+            if (timeMilli.length() > 1) {
+                timeMilli = timeMilli.substring(1);
             }
-            @Override
-            public void onFinish() {
-                timeKeeper.setText("0.0");
-            }
+            timeKeeper.setText(timeSec + "." + timeMilli);
+
+        }
+        @Override
+        public void onFinish() {
+            timeKeeper.setText("0.0");
+        }
 
     }
 
-    public void roundToast() {
-        Toast toast = new Toast(PlayActivity.this);
-    }
-
-    public void newRound() {
-        roundCount++;
-        roundKeeper.setText("" + roundCount);
-        roundToast();
-    }
-
-    public void endRound() {
-
-    }
 
 
     @Override
